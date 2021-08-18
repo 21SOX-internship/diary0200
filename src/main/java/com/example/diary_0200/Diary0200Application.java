08bdbe702aeb3c88a3af493179c99b84e4b2a62f
seongjun <qkrtjdwns03@naver.com>
2021-08-18 10:57:26 +0900

    의미 없는 업데이트

--- src/main/java/com/example/diary_0200/Diary0200Application.java
+++ src/main/java/com/example/diary_0200/Diary0200Application.java
@@ -1,7 +1,14 @@
 package com.example.diary_0200;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.boot.SpringApplication;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Diary0200Application {
@@ -9,5 +16,14 @@ public class Diary0200Application {
     public static void main(String[] args) {
         SpringApplication.run(Diary0200Application.class, args);
     }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res); return sessionFactory.getObject();
+    }
+
 
 }
